efa12dddd209a3069fd97a4dbdf31786caa7255f
Belagir <belagir.tedab@gmail.com>
2024-11-19 17:55:51 +0100

    work on recursive descent parser

--- src/dicelang/parser.c
+++ src/dicelang/parser.c
@@ -7,18 +7,29 @@
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 
-typedef bool (*parser_func)(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
-typedef RANGE(parser_func) RANGE_PARSER_F;
+enum parser_element_flavour {
+    PARSER_ELEMENT_func,
+    PARSER_ELEMENT_val,
+};
 
-static size_t expect(RANGE_TOKEN *tokens, size_t offset, size_t nb_elements, parser_func *what);
+struct parser_element {
+    enum parser_element_flavour flavour;
+    union {
+        bool (*func)(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
+        enum dicelang_token_flavour val;
+    };
+};
 
+// -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 
 static bool assignment(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
 static bool variable(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
-static bool designator(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
 static bool expression(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
-static bool identifier(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens);
+
+// -------------------------------------------------------------------------------------------------
+
+static size_t expect(RANGE_TOKEN *tokens, size_t offset, size_t nb_elements, struct parser_element *what);
 
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
@@ -31,47 +42,13 @@ static bool identifier(RANGE_TOKEN *tokens, size_t index, size_t *out_read_token
  */
 void dicelang_parse(RANGE_TOKEN *tokens, struct allocator alloc)
 {
-    assignment(tokens, 0, &(size_t) { 0 });
+    printf("%d\n", expect(tokens, 0, 2, (struct parser_element[]) {
+            { PARSER_ELEMENT_func, .func = &assignment },
+            { PARSER_ELEMENT_val, .val   = DTOK_line_end },
+    }));
 }
 
 // -------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------
-
-/**
- * @brief
- *
- * @param tokens
- * @param offset
- * @param nb_elements
- * @param what
- * @return u32
- */
-static size_t expect(RANGE_TOKEN *tokens, size_t offset, size_t nb_elements, parser_func *what)
-{
-    size_t pos = 0;
-    size_t read_tokens = 0;
-    size_t all_read_tokens = 0;
-    bool matches = false;
-
-    if (!what || !nb_elements) {
-        return 0;
-    }
-
-    matches = true;
-    while ((pos < nb_elements) && matches) {
-        read_tokens = 0;
-        matches = what[pos](tokens, offset + pos, &read_tokens);
-
-        pos += 1;
-        all_read_tokens += read_tokens;
-    }
-
-    if (matches) {
-        return all_read_tokens;
-    }
-    return 0;
-}
-
 // -------------------------------------------------------------------------------------------------
 
 /**
@@ -84,9 +61,11 @@ static size_t expect(RANGE_TOKEN *tokens, size_t offset, size_t nb_elements, par
  */
 static bool assignment(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens)
 {
-    *out_read_tokens = expect(tokens, index, 3, (parser_func[]) { &variable, &designator, &expression });
-
-    if (*out_read_tokens) printf("assignment\n");
+    *out_read_tokens = expect(tokens, index, 3, (struct parser_element[]) {
+            { PARSER_ELEMENT_func, .func = &variable },
+            { PARSER_ELEMENT_val, .val   = DTOK_designator },
+            { PARSER_ELEMENT_func, .func = &expression },
+    });
 
     return *out_read_tokens > 0;
 }
@@ -101,9 +80,9 @@ static bool assignment(RANGE_TOKEN *tokens, size_t index, size_t *out_read_token
  */
 static bool variable(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens)
 {
-    *out_read_tokens = expect(tokens, index, 1, (parser_func[]) { &identifier });
-
-    if (*out_read_tokens) printf("variable\n");
+    *out_read_tokens = expect(tokens, index, 1, (struct parser_element[]) {
+            { PARSER_ELEMENT_val, .val =  DTOK_identifier },
+    });
 
     return *out_read_tokens > 0;
 }
@@ -116,51 +95,76 @@ static bool variable(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens)
  * @return true
  * @return false
  */
-static bool designator(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens)
-{
-    if (!tokens || !tokens->data || (index >= tokens->length)) {
-        return false;
-    }
-
-    *out_read_tokens = (tokens->data[index].flavour == DTOK_designator);
-    if (*out_read_tokens) printf("designator\n");
-    return *out_read_tokens;
-}
-
-/**
- * @brief
- *
- * @param tokens
- * @param index
- * @return true
- * @return false
- */
 static bool expression(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens)
 {
-    if (!tokens || !tokens->data || (index >= tokens->length)) {
-        return false;
+    if (!*out_read_tokens) {
+        *out_read_tokens = expect(tokens, index, 3, (struct parser_element[]) {
+                { PARSER_ELEMENT_val, .val = DTOK_value },
+                { PARSER_ELEMENT_val, .val = DTOK_d },
+                { PARSER_ELEMENT_val, .val = DTOK_value },
+        });
     }
 
-    *out_read_tokens = tokens->data[index].flavour == DTOK_value;
-    if (*out_read_tokens) printf("expression\n");
-    return *out_read_tokens;
+    if (!*out_read_tokens) {
+        *out_read_tokens = expect(tokens, index, 3, (struct parser_element[]) {
+                { PARSER_ELEMENT_val, .val = DTOK_value },
+                { PARSER_ELEMENT_val, .val = DTOK_addition },
+                { PARSER_ELEMENT_val, .val = DTOK_value },
+        });
+    }
+
+    if (!*out_read_tokens) {
+        *out_read_tokens = expect(tokens, index, 1, (struct parser_element[]) {
+                { PARSER_ELEMENT_val, .val   = DTOK_value },
+        });
+    }
+
+    return *out_read_tokens > 0;
 }
+
+// -------------------------------------------------------------------------------------------------
 
 /**
  * @brief
  *
  * @param tokens
- * @param index
- * @return true
- * @return false
+ * @param offset
+ * @param nb_elements
+ * @param what
+ * @return u32
  */
-static bool identifier(RANGE_TOKEN *tokens, size_t index, size_t *out_read_tokens)
+static size_t expect(RANGE_TOKEN *tokens, size_t offset, size_t nb_elements, struct parser_element *what)
 {
-    if (!tokens || !tokens->data || (index >= tokens->length)) {
-        return false;
+    size_t pos = 0;
+    size_t read_tokens = 0;
+    size_t all_read_tokens = 0;
+    bool matches = false;
+
+    if (!what || !nb_elements) {
+        return 0;
     }
 
-    *out_read_tokens = tokens->data[index].flavour == DTOK_identifier;
-    if (*out_read_tokens) printf("identifier\n");
-    return *out_read_tokens;
+    matches = true;
+    while ((pos < nb_elements) && matches) {
+        read_tokens = 0;
+
+        switch (what[pos].flavour)
+        {
+            case PARSER_ELEMENT_func:
+                matches = what[pos].func(tokens, offset + pos, &read_tokens);
+                break;
+            case PARSER_ELEMENT_val:
+                matches = (what[pos].val == tokens->data[offset + pos].flavour);
+                read_tokens += matches;
+                break;
+        }
+
+        pos += 1;
+        all_read_tokens += read_tokens;
+    }
+
+    if (matches) {
+        return all_read_tokens;
+    }
+    return 0;
 }
